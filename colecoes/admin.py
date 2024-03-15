89abfb56a96e58ae347d66d0d65e52b6b9b69f0d
Luan Pimenta Fernandes <pimentaluan2005@gmail.com>
2024-03-14 23:57:25 -0300

    Model publicacao

--- colecoes/admin.py
+++ colecoes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from colecoes.models import Publicacao
 
-# Register your models here.
+class ListaPublicacoes(admin.ModelAdmin):
+    list_display = ("id","autor", "texto", "data_publicacao", "publicada")
+    list_display_links = ("id", "autor")
+    search_fields = ("autor",)
+    list_filter = ("publicada",)
+    list_editable = ("publicada",)
+    list_per_page = 10
+
+admin.site.register(Publicacao, ListaPublicacoes)
--- colecoes/models.py
+++ colecoes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Publicacao(models.Model):
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='publicacoes')
+    texto = models.TextField()
+    imagem = models.ImageField(upload_to='publicacoes/', null=True, blank=True)
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+    publicada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Publicação por {self.autor.username} em {self.data_publicacao}'
+
+@receiver(post_save, sender=Publicacao)
+def definir_autor(sender, instance, created, **kwargs):
+    if created:
+        instance.autor = get_user_model().objects.get(id=instance.autor.id)
+        instance.save()

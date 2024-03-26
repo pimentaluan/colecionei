baa0ce5d713af9111bbd8f2506bf384971134181
Luan Pimenta Fernandes <pimentaluan2005@gmail.com>
2024-03-26 01:43:32 -0300

    Icone para com o mouse

--- setup/static/scripts/login.js
+++ setup/static/scripts/login.js
@@ -30,9 +30,19 @@ icones.forEach(icone => {
 
         // Define uma posição inicial aleatória e uma velocidade de queda aleatória
         novaIcone.style.left = Math.random() * document.querySelector('.chuva-icones').offsetWidth + 'px';
-        novaIcone.style.animationDuration = Math.random() * 2 + 1 + 's'; // Entre 1 e 3 segundos
+        novaIcone.style.animationDuration = Math.random() * 6 + 2   + 's'; // Entre 2 e 8 segundos
 
         // Adiciona a nova ícone ao contêiner
         document.querySelector('.chuva-icones').appendChild(novaIcone);
+
+        // Pausa a animação quando o mouse está sobre o ícone
+        novaIcone.addEventListener('mouseover', function() {
+            this.style.animationPlayState = 'paused';
+        });
+
+        // Retoma a animação quando o mouse sai do ícone
+        novaIcone.addEventListener('mouseout', function() {
+            this.style.animationPlayState = 'running';
+        });
     }
 });

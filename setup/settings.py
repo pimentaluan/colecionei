856996b2ea0e60e5c6422c8e18560978d5c2a167
Luan Pimenta <luan.pimenta@academico.ifpb.edu.br>
2024-05-15 11:39:25 -0300

    Progestsql

--- setup/settings.py
+++ setup/settings.py
@@ -102,8 +102,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'wkmLFtGwDVTfXzucsusjDxGOckdfagRD',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '22739',     
     }
 }
 

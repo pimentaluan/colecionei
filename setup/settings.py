6390fbc0210f1702bec4256cfa93e608b4b91bef
Luan Pimenta Fernandes <pimentaluan2005@gmail.com>
2024-07-04 22:49:32 -0300

    tirando a config da AWS

--- setup/settings.py
+++ setup/settings.py
@@ -147,39 +147,15 @@ USE_TZ = True
 BRANCH_NAME = os.getenv('BRANCH_NAME')
 
 # Verifica se a variável de ambiente BRANCH_NAME está definida
-if BRANCH_NAME == 'develop':
-    # Configurações para a branch de desenvolvimento
-    STATIC_URL = 'static/'
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'setup/static')
-    ]
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-    MEDIA_URL = "/fotos/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'fotos')
+MEDIA_URL = "/fotos/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'fotos')
     
-else:
-    # Configurações para a branch de deploy
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
-    AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
-    AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400',
-    }
-    AWS_DEFAULT_ACL = None
-    AWS_LOCATION = 'static'
-    AWS_QUERYSTRING_AUTH = False
-
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'setup/static')
-    ]
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

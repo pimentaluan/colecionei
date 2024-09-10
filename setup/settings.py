4749e84a821c4dbb6865f2e63c138304c0a14d3b
Luan Pimenta Fernandes <pimentaluan2005@gmail.com>
2024-09-10 00:52:24 -0300

    rewrites .vercel.app

--- setup/settings.py
+++ setup/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 MEDIA_URL = "/fotos/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'fotos')
 

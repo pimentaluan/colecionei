ab998117a9ace530792b1c8680992cbd52f46c8b
Luan Pimenta Fernandes <pimentaluan2005@gmail.com>
2024-10-27 20:51:39 -0300

    Update build script for Vercel compatibility

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 # build_files.sh
 echo "Running build script..."
-pip install -r requirements.txt
-python3.3.12.1 manage.py collectstatic --noinput
+pip3 install -r requirements.txt
+python manage.py collectstatic --noinput
